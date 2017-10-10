588ee8404c20c2442c4c1ae67916589e4aa7849d
Kate Shostak <katherine.shostak@gmail.com>
2017-10-10 19:01:18 +0300

    Add an option to search for notes with the given date

--- notes.py
+++ notes.py
@@ -3,6 +3,7 @@ import time
 import sys
 import traceback
 import logging
+from datetime import datetime
 
 from abstract_saver import AbstractSaver
 from abstract_extractor import AbstractExtractor
@@ -57,18 +58,19 @@ class NoteExtractor(AbstractExtractor):
         return [note.split(":") for note in notes]
 
     def _search_by_date(self, notes, date):
-        pass
+        return [note for note in notes if datetime.fromtimestamp(float(note[0])).date() == date]
 
     def _search_by_word(self, notes, keyword):
         return [note for note in notes if keyword in note[1]]
 
     def _print_notes(self, notes):
+        print("Here are your notes.")
         for note in notes:
             note_date = self._get_time_formated(float(note[0]))
             print(f"{note_date} : {note[1]}")
 
-    def _get_time_formated(self, timestamp):
-        return time.strftime("%a, %d %b %Y %H:%M", time.gmtime(timestamp))
+    def _get_time_formated(self, note_timestamp):
+        return time.strftime("%a, %d %b %Y %H:%M", time.gmtime(note_timestamp))
 
 
 def parse_arguments():
@@ -89,7 +91,17 @@ def parse_arguments():
 
     parser.add_argument(
         "--keyword",
-        default="Hello"
+        default=None
+    )
+
+    parser.add_argument(
+        "--date",
+        default=None
+    )
+
+    parser.add_argument(
+        "--show",
+        default=True
     )
 
     return parser.parse_args()
@@ -100,11 +112,20 @@ def main():
     note_to_add = args.add
     file_path = args.path
     keyword = args.keyword
+    date_to_search = args.date
+    show = args.show
+
+    if date_to_search:
+        day, month, year = date_to_search.split("/")
+        date_to_search = datetime(int(year), int(month), int(day)).date()
+
     note = Note(note_to_add)
     note_saver = NoteSaverTxt(file_path)
     note_saver.save(note)
+
     note_extractor = NoteExtractor(file_path)
-    note_extractor.show_all_notes(word=keyword)
+    if show:
+        note_extractor.show_all_notes(keyword, date_to_search)
 
 
 if __name__ == "__main__":
